da292559088dce9bd3ae991dba36d875c790a12f
Mustafa744 <mustafaashraf744@gmail.com>
2023-09-08 21:51:29 +0300

    ..

--- train.py
+++ train.py
@@ -13,12 +13,12 @@ data_dir = f'{data_dir}/'
 
 use_tpu = vh.parameters('use_tpu').value
 train_steps = vh.parameters('train_steps').value
-model_dir = vh.parameters('model_dir').value
+# model_dir = vh.parameters('model_dir').value
 num_label_classes = vh.parameters('num_label_classes').value
 train_batch_size = vh.parameters('train_batch_size').value
 
 print(data_dir)
-os.system(f"""python /home/tensorflow/models/research/models/official/efficientnet/main.py --use_tpu={use_tpu} --data_dir={data_dir}
-          --model_dir={model_dir} --train_steps={train_steps} --skip_host_call=true --num_label_classes={num_label_classes} --train_batch_size={train_batch_size} """)
+os.system(f"""python /home/tensorflow/models/research/models/official/efficientnet/main.py --use_tpu=false --data_dir={data_dir}
+          --model_dir='/home/tensorflow/models/research/new/' --train_steps={train_steps} --skip_host_call=true --num_label_classes={num_label_classes} --train_batch_size={train_batch_size} """)
 shutil.make_archive(vh.outputs('trained').path('efficientnet'), 'zip', "/home/tensorflow/models/research/new/")
 os.system(f"mv /home/tensorflow/models/research/new/ {vh.outputs('tfrecord').path('efficientnet')}")
