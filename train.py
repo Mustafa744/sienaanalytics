cca4a09669b358e720014fa5099f3915eca6c018
Mustafa744 <mustafaashraf744@gmail.com>
2023-09-09 00:14:59 +0300

    ..

--- train.py
+++ train.py
@@ -20,4 +20,4 @@ train_batch_size = vh.parameters('train_batch_size').value
 print(data_dir)
 os.system(f"""python /home/tensorflow/models/research/models/official/efficientnet/main.py --use_tpu=False --data_dir={data_dir} --model_dir=/home/tensorflow/models/research/new/ --train_steps=10 --skip_host_call=true --num_label_classes=5 --train_batch_size=8 """)
 shutil.make_archive(vh.outputs().path('efficientnet'), 'zip', "/home/tensorflow/models/research/new/")
-os.system(f"mv /home/tensorflow/models/research/new/ {vh.outputs('tfrecord').path('efficientnet')}")
+# os.system(f"mv /home/tensorflow/models/research/new/ {vh.outputs('tfrecord').path('efficientnet')}")
