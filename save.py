aef6558d46c7eb0def1f13eb02fced390c2933b8
Mustafa744 <mustafaashraf744@gmail.com>
2023-09-10 02:22:43 +0300

    ..

--- save.py
+++ save.py
@@ -1,32 +1,22 @@
 import os, glob
 import valohai as vh
+import re
 import tarfile
 import shutil
 import zipfile
 import time
 
-inputs = {
-    "model": "",
-}
+# inputs = {
+#     "model": "",
+# }
 
-vh.prepare(step="save step", image="mo0haned/net", default_inputs=inputs)
+# vh.prepare(step="save step", image="mo0haned/net", default_inputs=inputs)
+meta_file = ""
+checkpoint_dir = ""
 
 try:
-    print(vh.inputs("model").paths())
-except Exception as e:
-    print("error", e)
-try:
-    print(i for i in vh.inputs("model").paths())
-    for image_path in vh.inputs("model").paths():
-        print(image_path)
+    for file in vh.inputs("trained_model").paths():
+        print(file)
 
 except Exception as e:
     print("error", e)
-
-# try:
-#     unzip = zipfile.ZipFile(vh.inputs("model").path(), "r")
-#     unzip.extractall(vh.outputs().path("exctracted_model"))
-#     print("extracted model to: " + vh.outputs().path("exctracted_model"))
-# except Exception as e:
-#     print("error extracting model", e)
-#     print(os.listdir(vh.inputs("model").path()))
