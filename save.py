4b3884141b480329429b72338d50dcb8354a7b9e
Mustafa744 <mustafaashraf744@gmail.com>
2023-09-10 03:03:34 +0300

    ..

--- save.py
+++ save.py
@@ -8,12 +8,9 @@ import time
 import tensorflow as tf
 
 tf.compat.v1.disable_eager_execution()
-meta_file = "/home/tensorflow/models/research/new/model.ckpt-0.meta"
-checkpoint_dir = "/home/tensorflow/models/research/new/"
-print("using os")
-print(os.listdir("/home/tensorflow/models/research/new/"))
-print("using valohai")
-print(vh.inputs("trained_model").path("model.ckpt-0.meta"))
+meta_file = vh.inputs("trained_model").path("model.ckpt-0.meta")
+checkpoint_dir = vh.inputs("trained_model").path("efficientnet")
+
 saver = tf.compat.v1.train.import_meta_graph(meta_file)
 
 # checkpoint_dir = "/home/tensorflow/models/research/trained"
