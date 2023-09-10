7c7bded2e7492a1adec0d8d312787e27052e62f6
Mustafa744 <mustafaashraf744@gmail.com>
2023-09-11 01:45:17 +0300

    ..

--- eval.py
+++ eval.py
@@ -23,8 +23,8 @@ with vh.metadata.logger() as logger:
     logger.log("attempting to load model", vh.inputs("frozen_model").path())
     try:
         # Load the frozen graph
-        with tf.gfile.GFile("./frozen_graph.pb", "rb") as f:
-            graph_def = tf.GraphDef()
+        with tf.io.gfile.GFile("./frozen_graph.pb", "rb") as f:
+            graph_def = tf.compat.v1.GraphDef()
             graph_def.ParseFromString(f.read())
         logger.log("model loaded")
     except Exception as e:
