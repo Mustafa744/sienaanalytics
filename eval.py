adaa9e2e70a6a35d2a12a4912a4906e8534ac4e9
Mustafa744 <mustafaashraf744@gmail.com>
2023-09-11 05:06:56 +0300

    finaly it's working!

--- eval.py
+++ eval.py
@@ -1,24 +1,6 @@
 import valohai as vh
 import tensorflow as tf
 
-# # load frozen graph
-# with tf.gfile.GFile("./frozen_graph.pb", "rb") as f:
-#     graph_def = tf.GraphDef()
-#     graph_def.ParseFromString(f.read())
-
-# # print graph information
-# for node in graph_def.node:
-#     print(node)
-# #####################################################################
-# import tensorflow as tf
-
-
-# # Create a summary file to describe the graph
-# summary_file_path = "./graph_summary.pbtxt"
-# with open(summary_file_path, "w") as summary_file:
-#     summary_file.write(str(graph_def))
-
-
 with vh.metadata.logger() as logger:
     logger.log("attempting to load model", vh.inputs("frozen_model").path())
     try:
